be7a7a8dd895a3b3a7e34d68a8828d69a185abb3
jewelnaresh <jewelnaresh2@gmail.com>
2019-04-19 04:56:00 +0530

    messages in general can be seen when the page is loaded

--- application.py
+++ application.py
@@ -1,20 +1,34 @@
 import os
 
 from flask import Flask, render_template, request
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, join_room, leave_room
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
 channels = {}
-channels["general"] = []
+channels["general"] = [dict(msg="HI", username="asdd", time="12:000pm")]
 channellist = ["general"]
 
 
 @app.route("/")
 def index():
     return render_template("index.html", channellist=channellist)
+
+@socketio.on("join channel")
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    send(username + ' has entered the room.', room=room)
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send(username + ' has left the room.', room=room)
 
 
 @socketio.on("add channel")
@@ -28,3 +42,16 @@ def addchannel(channelname):
     channels[channelname] = []
 
     emit("new channel", {"channelname": channelname, "error": error}, broadcast=True)
+
+
+@socketio.on("show messages")
+def showmessages(channelname):
+    channelname = channelname[1:]
+    emit("channel messages", channels[channelname])
+
+
+@socketio.on("channel selected")
+def channelselected(channelname):
+    channelname = channelname[1:]
+    channellist = list(channels.keys())    
+    emit("channel messages", channels[channelname], room=channellist[channellist.index(channelname)])
--- static/script.js
+++ static/script.js
@@ -32,6 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
         element.addEventListener("click", () => {
             document.querySelector(".active").classList.remove("active");
             element.classList.add("active");
+            socket.emit("channel selected", document.querySelector(".active").innerHTML);
         });
     });
 
@@ -69,6 +70,53 @@ document.addEventListener('DOMContentLoaded', () => {
                 document.querySelector(".active").classList.remove("active");
                 btn.classList.add("active");
             });
+        }
+    });
+
+    // show messages
+    socket.on("connect", () => {
+        socket.emit("join channel", {room: document.querySelector(".active").innerHTML, username: localStorage.getItem(username));
+        socket.emit("show messages", document.querySelector(".active").innerHTML);
+    });
+
+    socket.on("channel messages", (messages) => {
+
+        for (const data of messages ) {
+            let div_outer = document.createElement("div");
+            let div_middle = document.createElement("div");
+            let div_inner = document.createElement("div");
+            let p_username = document.createElement("p");
+            let p_msg = document.createElement("p");
+            let p_time = document.createElement("p");
+    
+            div_outer.classList.add("msg-container");
+            div_middle.classList.add("msg");
+    
+            p_username.classList.add("msg-sender");
+            p_msg.classList.add("msg-text");
+            p_time.classList.add("timestamp");
+            
+            if (data["username"] === localStorage.getItem(username)) {
+                div_middle.classList.add("msg-right");
+                div_inner.classList.add("msg-sent");
+                p_username.classList.add("msg-sender-self");
+                p_time.classList.add("msg-right");
+            }
+            else {
+                div_inner.classList.add("msg-received");
+                p_username.classList.add("msg-sender-other");
+            }
+
+            p_username.appendChild(document.createTextNode(data["username"]));
+            p_msg.appendChild(document.createTextNode(data["msg"]));
+            p_time.appendChild(document.createTextNode(data["time"]));
+
+            document.querySelector("#chatbox").appendChild(div_outer);
+            div_outer.appendChild(div_middle);
+            div_outer.appendChild(p_time);
+            div_middle.appendChild(p_username);
+            div_middle.appendChild(div_inner);
+            div_inner.appendChild(p_msg);
         }
     });
 
